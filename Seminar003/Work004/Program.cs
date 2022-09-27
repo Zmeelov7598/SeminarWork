aeede9b3f194c6effa41b68aac92b1421e68de65
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-09-27 18:52:23 +0300

    Finish Work003

---
+++ Seminar003/Work004/Program.cs
@@ -0,0 +1,16 @@
+﻿int Number()
+{
+    Console.WriteLine("Введите число! ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetPrintPow(int number)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.Write(" " + Math.Pow(count, 2));
+        count++;
+    }
+}
+GetPrintPow(Number());
