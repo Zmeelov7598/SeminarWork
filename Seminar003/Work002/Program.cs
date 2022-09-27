a5e298435d951ad02ee9c121a6d7e0cd45bad15e
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-09-27 18:11:46 +0300

    Finish Work002

--- Seminar003/Work002/Program.cs
+++ Seminar003/Work002/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿double GetNumber(string name)
+{
+    Console.WriteLine("Введите координату   " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+int GetNumChetv(double x, double y)
+{
+    if (x > 0 && y > 0)
+        return 1;
+    else
+    if (x < 0 && y >0)
+        return 2;
+    else
+    if (x < 0 && y < 0)
+        return 3;
+    else return 4;
+}
+
+double x = GetNumber("x");
+double y = GetNumber("y");
+
+Console.WriteLine("Четверть: " + GetNumChetv(x,y));
