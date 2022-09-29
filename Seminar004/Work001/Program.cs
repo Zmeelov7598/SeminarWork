63c13fac88273d2a7b3671abdde18870ffefbf14
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-09-29 17:59:38 +0300

    Start Seminar004

---
+++ Seminar004/Work001/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetNumberThreeDigits()
+{
+    int num = 0;
+    while(num < 99 || num > 999)
+    {
+        Console.WriteLine("Введите трехзначное число!");
+        Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+int a = GetNumberThreeDigits();
+
+Console.WriteLine(a);
