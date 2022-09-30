2173312078d167d99c6d8c2f7f605ffcc1c61cdd
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-09-30 18:25:28 +0300

    Finish Work005

---
+++ Seminar004/Work005/Program.cs
@@ -0,0 +1,8 @@
+﻿// выводит массив заполняя нулями и единицами рамдомно
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 2);
+    Console.Write(array[i] + " ");
+}
