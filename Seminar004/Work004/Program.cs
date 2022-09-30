c63f4775bdc16edcfbaf62d8b24c1f8a8ec5bf03
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-09-30 18:12:06 +0300

    Finish work004

---
+++ Seminar004/Work004/Program.cs
@@ -0,0 +1,19 @@
+﻿// вход число N и Выдает произвдение чисел от 1 до N;
+// 4 => 24
+int GetNumber()
+{
+    Console.WriteLine("Введите число N: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetProizv(int number)
+{
+    int proizv = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        proizv = proizv * i;
+    }
+    return proizv;
+}
+int num = GetNumber();
+Console.WriteLine("Произведение цифр равно " + GetProizv(num));
