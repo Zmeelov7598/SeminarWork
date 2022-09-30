3bbc124ca582362e246d5516e44a39dd8b13f058
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-09-30 17:38:29 +0300

    Finish Work003

---
+++ Seminar004/Work003/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу которое на вход принимает число и выдает количество цифр в числе
+//456 => 3
+int InputNumber()
+{
+    Console.WriteLine("Введите сило!");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetDigCount(int Number)
+{
+    int DigCount = 0;
+   // Number = Math.Abs(Number/10);
+    while (Math.Abs(Number) > 0)
+    {
+        Number = Number / 10;
+        DigCount++;
+    }
+    return DigCount;
+}
+Console.WriteLine("Количество цифр в числе " + GetDigCount(InputNumber()));
