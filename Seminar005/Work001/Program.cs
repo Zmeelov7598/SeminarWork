4d7cd383609f6ac485b3612bb869b7b3b2aadb73
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-02 20:23:31 +0300

    Don't Finish Work001

---
+++ Seminar005/Work001/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int GetSumPositive(int[] arr)
+{
+    int sum = 0;
+    for (i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            sum += arr[i];
+    }
+    return sum;
+}
+
+int[] massiv = new int[12];
+FillArray(massiv);
+PrintArray(massiv);
