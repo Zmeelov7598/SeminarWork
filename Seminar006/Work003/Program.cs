f1f9a01e3fad0a9e5573a0e37c7d8c8c4b0f855d
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-07 20:22:12 +0600

    Finish Work003

---
+++ Seminar006/Work003/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу которая будет преобразовывать десятичное исло в двоичное.
+// 45--101101
+//--11
+// 2--10
+
+int a = Convert.ToInt32(Console.ReadLine());
+int temp = a;
+int index = 0;
+while (temp > 0)
+{
+    index++;
+    temp /= 2;
+}
+int[] arr = new int[index];
+//Console.WriteLine(index);
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = a % 2;
+    a /= 2;
+}
+void SwapArrayTwo(int[] arr)
+{
+    int swap;
+    for (int i = 0; i < arr.Length / 2; i++)
+    {
+        swap = arr[arr.Length - i - 1];
+        arr[arr.Length-i-1]=arr[i];
+        arr[i]=swap;
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+SwapArrayTwo(arr);
+PrintArray(arr);
