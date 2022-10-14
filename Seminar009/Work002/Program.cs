b57f7411ffce1b6c552f6ead61beae1944a82984
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-14 10:55:50 +0600

    Finish Work002

---
+++ Seminar009/Work002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 63: Задайте значение вещественное N. Напишите программу
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N=5->"1, 2, 3, 4, 5"
+
+double GetNumberFromConsole()
+{
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+string WriteNumbersRecursion(int startNumber, double lastNumber)
+{
+    if (startNumber <=lastNumber)
+        return startNumber + ", " + WriteNumbersRecursion(startNumber + 1, lastNumber);
+    else
+        return string.Empty;
+}
+
+Console.Write(WriteNumbersRecursion(1, GetNumberFromConsole()));
+Console.WriteLine();
